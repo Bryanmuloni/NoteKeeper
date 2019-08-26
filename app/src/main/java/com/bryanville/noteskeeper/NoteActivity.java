e661874055aecb633374de5f285414a6dc453a8a
Sirikye Brian <bryanmuloni@gmail.com>
2019-08-26 14:29:21 +0300

    Exposing data outside android application,Module Notifications completed

--- app/src/main/java/com/bryanville/noteskeeper/NoteActivity.java
+++ app/src/main/java/com/bryanville/noteskeeper/NoteActivity.java
@@ -14,15 +14,12 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -263,13 +260,25 @@ public class NoteActivity extends AppCompatActivity implements LoaderManager.Loa
         }
         else if (id == R.id.action_next){
             moveNext();
-        }
-        else if (id == R.id.action_cancel){
+        } else if (id == R.id.action_cancel) {
             mIsCancelling = true;
             finish();
+        } else if (id == R.id.action_set_reminder) {
+            showReminderNotification();
+            Toast.makeText(this, "Wow", Toast.LENGTH_SHORT).show();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showReminderNotification() {
+        Log.d(LOG_TAG, "showReminderNotification: calling notify method from notification helper " +
+                "class");
+        String noteTitle = textNoteTitle.getText().toString();
+        String noteText = textNoteText.getText().toString();
+
+        int noteId = (int) ContentUris.parseId(mNoteUri);
+        NoteReminderNotification.notify(this, noteTitle, noteText, noteId);
     }
 
     @Override
@@ -292,17 +301,17 @@ public class NoteActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
     private void sendEmail() {
-        CourseInfo course = (CourseInfo) mSpinnerCourses.getSelectedItem();
-        String subject = textNoteTitle.getText().toString();
-        String text = "Checkout what I learnt in the PluralSight course " +
-                course.getTitle() + "\n" + textNoteText.getText().toString();
-
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("message/rfc2822");
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        intent.putExtra(Intent.EXTRA_TEXT, text);
-
-        startActivity(intent);
+//        CourseInfo course = (CourseInfo) mSpinnerCourses.getSelectedItem();
+//        String subject = textNoteTitle.getText().toString();
+//        String text = "Checkout what I learnt in the PluralSight course " +
+//                course.getTitle() + "\n" + textNoteText.getText().toString();
+//
+//        Intent intent = new Intent(Intent.ACTION_SEND);
+//        intent.setType("message/rfc2822");
+//        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+//        intent.putExtra(Intent.EXTRA_TEXT, text);
+//
+//        startActivity(intent);
     }
 
 
